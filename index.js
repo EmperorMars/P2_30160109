50802244fdb78b4188447d3b779e0d53c4b71e84
EmperorMars <marcosvas2004@gmail.com>
2025-06-29 20:08:53 +0000

    malvo

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-require("dotenv").config();
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
-const patch = require("path");
-const layouts = require("express-ejs-layouts");
-app.use(express.static(patch.join(__dirname, "public")));
 
-const main = require("./routes/main.routes");
-const productos = require("./routes/productos.routes");
-
-app.use(layouts);
-
+// Configura el motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "src",'views')); 
 app.set("layout", "layouts/layout");
-app.use(layouts);
-app.set("view engine", "ejs");
-app.set('views', patch.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(patch.join(__dirname, "public")));
+
+// Configura express-ejs-layouts
+app.use(expressLayouts);
+
+// Middleware para parsear el cuerpo de las solicitudes
 app.use(express.urlencoded({ extended: true }));
-app.use(main);
-app.use(productos);
+app.use(express.json());
 
+// Rutas
+const mainController = require('./controllers/main.controllers');
+app.get('/', mainController.index);
+app.post('/submit-comentario', mainController.submitComentario);
+
+// Inicia el servidor
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
